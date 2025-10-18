a29448d425d0449509b1bf1b73ed1944452be552
Jaeden Mah <jaedenmah@gmail.com>
2025-10-18 21:43:27 +0800

    Stuff

--- app/src/main/java/com/example/pocketlibrary/AddBookActivity.kt
+++ app/src/main/java/com/example/pocketlibrary/AddBookActivity.kt
@@ -14,6 +14,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import coil.load
 import com.example.pocketlibrary.databinding.ActivityAddBookBinding
 import com.google.firebase.Firebase
 import com.google.firebase.database.database
@@ -25,6 +26,7 @@ class AddBookActivity : AppCompatActivity() {
     lateinit var titleInput: EditText
     lateinit var authorInput: EditText
     lateinit var yearInput: EditText
+    private lateinit var coverView: ImageView
     lateinit var saveBtn: Button
     private lateinit var db: AppDatabase
     private lateinit var bookDAO: BookDAO
@@ -39,6 +41,7 @@ class AddBookActivity : AppCompatActivity() {
         titleInput = findViewById(R.id.title_text_input_layout)
         authorInput = findViewById(R.id.author_text_input_layout)
         yearInput = findViewById(R.id.year_text_input_layout)
+        coverView = findViewById<ImageView>(R.id.book_cover_imageview)
         saveBtn = findViewById(R.id.saveBtn)
 
         db = AppDatabase.getDatabase(this)
@@ -48,19 +51,17 @@ class AddBookActivity : AppCompatActivity() {
             val title = titleInput.text.toString().trim()
             val author = authorInput.text.toString().trim()
             val year = yearInput.text.toString().trim()
+            // Need to make handler to handle taking cover photo and then saving it here (probs not url). The handler can be an onlcicklistner on teh imagecover location
 
             if (title.isEmpty() || author.isEmpty() || year.isEmpty()) {
                 Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
             } else {
-                val book = Book(id = " ", title = title, author = author, year = year, addedManually = true)
+                val book = Book(id = " ", title = title, author = author, year = year, coverUrl = "", addedManually = true)
                 favouritesViewModel.addBook(book)
                     Toast.makeText(this@AddBookActivity, "Book saved", Toast.LENGTH_SHORT).show()
                     finish()
 
             }
-
-            // Need to also add implmentation that will add it to firebase
-
 
         }
     }
--- app/src/main/java/com/example/pocketlibrary/EditBookActivity.kt
+++ app/src/main/java/com/example/pocketlibrary/EditBookActivity.kt
@@ -7,12 +7,14 @@ import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import coil.load
 
 class EditBookActivity : AppCompatActivity() {
     lateinit var bookCoverInput: ImageView
     lateinit var titleInput: EditText
     lateinit var authorInput: EditText
     lateinit var yearInput: EditText
+    private lateinit var coverView: ImageView
     lateinit var saveBtn: Button
     private val favouritesViewModel: FavouritesViewModel by viewModels()
 
@@ -24,6 +26,7 @@ class EditBookActivity : AppCompatActivity() {
         bookCoverInput = findViewById(R.id.book_cover_imageview)
         titleInput = findViewById(R.id.title_text_input_layout)
         authorInput = findViewById(R.id.author_text_input_layout)
+        coverView = findViewById<ImageView>(R.id.book_cover_imageview)
         yearInput = findViewById(R.id.year_text_input_layout)
         saveBtn = findViewById(R.id.saveBtn)
 
@@ -35,8 +38,14 @@ class EditBookActivity : AppCompatActivity() {
                 titleInput.setText(book?.title)
                 authorInput.setText(book?.author)
                 yearInput.setText(book?.year)
+                coverView.load(book?.coverUrl) {
+                    placeholder(R.drawable.ic_launcher_background) // Optional: Show a default image while loading
+                    error(R.drawable.ic_launcher_foreground)       // Optional: Show an error image if the URL is bad or loading fails
+                }
             }
 
+
+            // handler for if user clicks button to take cover photo
 
             saveBtn.setOnClickListener {
                 val updatedBook = Book(bookId, titleInput.text.toString(), authorInput.text.toString(), yearInput.text.toString())
